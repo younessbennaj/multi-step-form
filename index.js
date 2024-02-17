4b122037274d1a5dc6b38352edb132feec4b8ab5
younessbennaj <youness.bennaj@gmail.com>
2024-02-17 14:36:53 +0100

    feat: add logic to handle onChange()

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 class Form {
-  constructor({ initialValues = {}, fields, form, onSubmit }) {
+  constructor({ initialValues = {}, fields, onSubmit }) {
     this.values = { ...initialValues };
-
-    form.addEventListener("submit", (event) => {
-      event.preventDefault();
-      onSubmit(this.values);
-    });
+    this.onSubmit = onSubmit;
+    this.disabled = true;
+    this.onChangeHandler = null;
+    this.validateHandler = null;
+    this.errors = null;
 
     fields.forEach((field) => {
       if (field.type === "input") {
@@ -18,12 +18,57 @@ class Form {
     });
   }
 
+  onChange(func) {
+    this.onChangeHandler = func;
+  }
+
+  validate(func) {
+    this.validateHandler = func;
+  }
+
+  handleSubmit() {
+    this.onSubmit(this.values);
+  }
+
+  _toggleDisabled() {
+    this.disabled = !Boolean(this.disabled);
+  }
+
+  _handleValuesChange() {
+    this.onChangeHandler && this.onChangeHandler();
+    this._handleErrors();
+    this._setErrors();
+  }
+
+  _setErrors() {
+    function insertAfter(referenceNode, newNode) {
+      referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    }
+    const errorsFieldName = Object.keys(this.errors);
+
+    errorsFieldName.forEach((name) => {
+      // insertAfter()
+      // const selector = `input[name=${name}]`;
+      // const el = document.querySelector(selector);
+      // const error = document.createTextNode("required");
+      // insertAfter(el, error);
+    });
+  }
+
+  _handleErrors() {
+    if (this.validateHandler) {
+      const errors = this.validateHandler(this.values);
+      this.errors = errors;
+    }
+  }
+
   _bindInputByName(fieldName) {
     const selector = `input[name=${fieldName}]`;
     const input = document.querySelector(selector);
 
     input.addEventListener("input", (event) => {
       this.values[fieldName] = event.currentTarget.value;
+      this._handleValuesChange();
     });
   }
 
@@ -37,14 +82,15 @@ class Form {
         const newSet = new Set(this.values[fieldName]);
         newSet.has(value) ? newSet.delete(value) : newSet.add(value);
         this.values[fieldName] = Array.from(newSet);
+
+        // UTILS
+        this._handleValuesChange();
       });
     });
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("form");
-
   const initialValues = {
     email: "",
     interests: [],
@@ -70,5 +116,34 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("values", values);
   };
 
-  const myForm = new Form({ initialValues, fields, form, onSubmit });
+  const validateHandler = (values) => {
+    const errors = {};
+
+    // if name is missing
+    if (!values.name) {
+      errors.name = "required";
+    }
+
+    return errors;
+  };
+
+  const myForm = new Form({ initialValues, fields, onSubmit });
+
+  const submitButton = document.getElementById("submit");
+
+  submitButton.setAttribute("disabled", "true");
+
+  submitButton.addEventListener("click", () => {
+    myForm.handleSubmit();
+  });
+
+  myForm.onChange(function () {
+    if (this.disabled) {
+      submitButton.setAttribute("disabled", "true");
+    } else {
+      submitButton.removeAttribute("disabled");
+    }
+  });
+
+  myForm.validate(validateHandler);
 });
